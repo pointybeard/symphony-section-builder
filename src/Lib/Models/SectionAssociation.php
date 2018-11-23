9c35a8e9823227cc4598fe376ef95b2df525151d
Alannah Kearney <alannah@ruleandmake.com>
2018-11-23 22:57:36 +0000

    Added SectionAssociation class to handle creation and deletion of section associations

---
+++ src/Lib/Models/SectionAssociation.php
@@ -0,0 +1,135 @@
+<?php
+namespace pointybeard\Symphony\SectionBuilder\Lib\Models;
+
+use pointybeard\Symphony\SectionBuilder\Lib\AbstractField;
+use pointybeard\PropertyBag\Lib;
+use SymphonyPDO\Lib\ResultIterator;
+use pointybeard\Symphony\SectionBuilder\Lib\AbstractTableModel;
+use pointybeard\Symphony\SectionBuilder\Lib\Exceptions;
+
+class SectionAssociation extends AbstractTableModel
+{
+    protected $fields = [];
+    const TABLE = 'tbl_sections_association';
+
+    public static function getFieldMappings()
+    {
+        return (object)[
+            'id' => [
+                'name' => 'id',
+                'flags' => self::FLAG_INT
+            ],
+
+            'parent_section_id' => [
+                'name' => 'parentSectionId',
+                'flags' => self::FLAG_SECTION
+            ],
+
+            'parent_section_field_id' => [
+                'name' => 'parentSectionFieldId',
+                'flags' => self::FLAG_FIELD | self::FLAG_NULL,
+            ],
+
+            'child_section_id' => [
+                'name' => 'childSectionId',
+                'flags' => self::FLAG_SECTION
+            ],
+
+            'child_section_field_id' => [
+                'name' => 'childSectionFieldId',
+                'flags' => self::FLAG_FIELD
+            ],
+
+            'hide_association' => [
+                'name' => 'hideAssociation',
+                'flags' => self::FLAG_BOOL,
+                'default' => false
+            ],
+
+            'interface' => [
+                'name' => 'interface',
+                'flags' => self::FLAG_STR | self::FLAG_NULL
+            ],
+
+            'editor' => [
+                'name' => 'editor',
+                'flags' => self::FLAG_STR | self::FLAG_NULL
+            ],
+        ];
+    }
+
+    private function fetchFieldOrSection($field, $type) {
+        return (
+            $this->$field->value instanceof $type
+                ? $this->$field->value
+                : $type::loadFromId($this->$field->value)
+        );
+    }
+
+    public function parentSection() {
+        return $this->fetchFieldOrSection('parentSectionId', __NAMESPACE__ . '\\Section');
+    }
+
+    public function childSection() {
+        return $this->fetchFieldOrSection('childSectionId', __NAMESPACE__ . '\\Section');
+    }
+
+    public function parentSectionField() {
+        return $this->fetchFieldOrSection('parentSectionFieldId', preg_replace("@\\\\Models$@", '', __NAMESPACE__) . '\\AbstractField');
+    }
+
+    public function childSectionField() {
+        return $this->fetchFieldOrSection('childSectionFieldId', preg_replace("@\\\\Models$@", '', __NAMESPACE__) . '\\AbstractField');
+    }
+
+    public function getDatabaseReadyData()
+    {
+        return [
+            'interface' => (string)$this->interface,
+            'editor' => (string)$this->editor,
+            'hide_association' => self::boolToEnumYesNo($this->hideAssociation->value),
+            'parent_section_id' => (int)$this->parentSection()->id->value,
+            'child_section_id' => (int)$this->childSection()->id->value,
+            'parent_section_field_id' => (int)$this->parentSectionField()->id->value,
+            'child_section_field_id' => (int)$this->childSectionField()->id->value,
+        ];
+    }
+
+    protected static function boolToEnumYesNo($value)
+    {
+        return $value == true ? 'yes' : 'no';
+    }
+
+    public static function fetchByChildSectionId($sectionId)
+    {
+        $query = \SymphonyPDO\Loader::instance()->prepare(sprintf(
+            'SELECT * FROM `%s` WHERE `child_section_id` = :sectionId LIMIT 1',
+            static::TABLE
+        ));
+        $query->bindParam(':sectionId', $sectionId, \PDO::PARAM_INT);
+        $result = $query->execute();
+
+        return (new ResultIterator(
+            get_called_class(),
+            $query
+        ));
+    }
+
+    public function commit()
+    {
+        $data = self::getDatabaseReadyData();
+        $table = self::TABLE;
+        \SymphonyPDO\Loader::instance()->doInTransaction(
+            function(\SymphonyPDO\Lib\Database $db) use ($table, $data) {
+                $db->delete($table, sprintf(
+                    "`child_section_id` = %d AND `child_section_field_id` = %d",
+                    $data['child_section_id'],
+                    $data['child_section_field_id']
+                ));
+                $db->insert($data, self::TABLE);
+                return true;
+            }
+        );
+        return $this;
+    }
+}
